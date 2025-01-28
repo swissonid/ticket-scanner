cc1f1fcc0caa622f788b02b00f0075d83f9c2d70
Patrice Mueller <patrice.mueller@zuhlke.com>
2025-01-28 23:29:38 +0100

    more linting

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import MovieList from '@/features/qrcde-scanner/screens/movie-list';
-import MoviePage from '@/features/qrcde-scanner/screens/movies-page';
 
 export default function Page() {
   return MovieList();
