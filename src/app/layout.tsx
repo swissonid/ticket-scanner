91169450bb64d2a6181f69e2c278a3e56c35723e
Patrice Mueller <patrice.mueller@zuhlke.com>
2025-01-30 16:37:56 +0100

    should fix layout shift

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} h-svh antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} h-dvh antialiased`}
       >
         {children}
       </body>
